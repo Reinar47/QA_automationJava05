5f280fa9fb5c9fee329d698d4a59b0b0ff73fa50
Igor K <reinar4747@gmail.com>
2022-10-10 15:25:29 +0300

    3

--- src/GitTest11.java
+++ src/GitTest11.java
@@ -1,6 +1,6 @@
 public class GitTest11 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello hello hello");
         System.out.println("hello1");
 
     }
